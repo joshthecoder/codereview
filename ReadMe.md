f85e1a76e5ec4a2a62eb1704c76a2088aaaec9df
Joshua Roesslein <jroesslein@appcelerator.com>
2011-08-22 16:23:54 -0500

    Layout project folders. Add setup script.

--- ReadMe.md
+++ ReadMe.md
@@ -2,4 +2,3 @@ Code Review via Github Pull Requests
 ====================================
 A tool for performing code review using Github's pull requests.
 
-
---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+#from distutils.core import setup
+from setuptools import setup, find_packages
+
+setup(name="github_codereview",
+      version="1.0.0",
+      description="",
+      license="MIT",
+      author="Appcelerator, Inc.",
+      author_email="jroesslein@appcelerator.com",
+      url="http://github.com/appcelerator/codereview",
+      packages=['codereview'],
+      scripts=['bin/codereview'],
+      keywords= "code review github pull requests",
+      install_requires=['requests'],
+      zip_safe = True)
